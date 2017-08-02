e680eae7c28791b729f67c3798a7ea32eee50a0c
yann <yann.boisselier@gmail.com>
2017-08-02 17:59:07 +0200

    cleaning

--- components/PostDetail.vue
+++ components/PostDetail.vue
@@ -1,10 +1,6 @@
 <template>
   <section class="container content post section">
     <h1 class="title is-3" v-html="post.title.rendered"></h1>
-    <!--
-    <div class="date">écrit le {{post.date}}</div>
-    <div class="date">édité le {{post.modified}}</div>
-    -->
     <div class="featured-media"><img :src="post.featured_media_url"/></div>
     <div v-html="post.content.rendered" class="content"></div>
     <div v-if="post._embedded['wp:term'][1]" class="tags">
--- components/Posts.vue
+++ components/Posts.vue
@@ -1,29 +1,24 @@
 <!-- Liste des posts de blogs -->
 <template>
-  <div class="posts">
-    <section class="section">
-      <div v-for="post in posts.data" class="post">
+  <section class="posts section">
+    <div v-for="post in posts.data" class="post">
   
-        <nuxt-link class="title is-3" :to="{ name: 'blog-slug', params: { slug: post.slug, post: post } }">
-          <h2 v-html="post.title.rendered"></h2>
-        </nuxt-link>
+      <nuxt-link class="title is-3" :to="{ name: 'blog-slug', params: { slug: post.slug, post: post } }">
+        <h2 v-html="post.title.rendered"></h2>
+      </nuxt-link>
   
-        <img :src="post.featured_media_url" />
+      <img v-if="post.featured_media_url" :src="post.featured_media_url" />
   
-        <div class="content" v-html="post.excerpt.rendered"></div>
-        <div>
-          <nuxt-link class="button is-primary" :to="{ name: 'blog-slug', params: { slug: post.slug, post: post } }"> Lire l'article </nuxt-link>
-        </div>
-        <hr />
+      <div class="content" v-html="post.excerpt.rendered"></div>
+      <div>
+        <nuxt-link class="button is-primary" :to="{ name: 'blog-slug', params: { slug: post.slug, post: post } }"> Lire l'article </nuxt-link>
       </div>
+      <hr />
+    </div>
   
-    </section>
+    <Pagination :totalPages="posts.totalPages" />
   
-    <section class="section">
-      <Pagination :totalPages="posts.totalPages" />
-    </section>
-  
-  </div>
+  </section>
 </template>
 
 <script>
