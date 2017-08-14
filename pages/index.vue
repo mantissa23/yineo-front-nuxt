9daafe9dd8b59bcbbe8f54cd50062c158388a948
yann <yann.boisselier@gmail.com>
2017-08-14 17:23:56 +0200

    remove getPost call from homepage, sidebar posts are already fetched by the store

--- pages/index.vue
+++ pages/index.vue
@@ -4,15 +4,9 @@
 
 <script>
 import Presentation from '~/components/Presentation'
-import { getPosts } from '~/services/wpContentApi'
 
 export default {
   transition: 'page',
-  components: { Presentation },
-  async asyncData () {
-    return {
-      posts: await getPosts(10)
-    }
-  }
+  components: { Presentation }
 }
 </script>
