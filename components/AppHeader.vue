87658b1b1f7079939e33ac518303289c8199af13
yann <yann.boisselier@gmail.com>
2017-08-17 15:11:25 +0200

    add svg logo

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -2,6 +2,7 @@
   <section class="hero is-primary is-hidden-mobile is-hidden-touch">
     <div class="hero-body">
       <div class="container">
+        <img class="logo" src="~/assets/src/Yineo_logo.svg" />
         <h1 class="title">
           Yann Boisselier <br />
           Développeur freelance à Nantes
@@ -13,4 +14,10 @@
     </div>
   </section>
 </template>
+
+<style scoped>
+.logo {
+  height:200px;
+}
+</style>
 
