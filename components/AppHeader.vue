0cc0736f9e3fce47e73f1eb928e42d2293aa53ab
Floris Moriceau <floris.moriceau@komuneco.org>
2017-08-17 15:56:13 +0200

    menu

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,14 +1,19 @@
 <template>
   <section class="hero is-hidden-mobile">
-      <div class="container main-header has-text-centered">
-        <img class="logo" src="~/assets/src/Yineo_logo.svg" alt="Yineo"/>
-      </div>
+    <div class="container main-header has-text-centered">
+      <img class="logo" src="~/assets/src/Yineo_logo.svg" alt="Yineo" />
+    </div>
   </section>
 </template>
 
 <style scoped>
-.logo {
-  height:30vh;
-}
+  .main-header {
+    padding: 5rem 0 2rem 0;
+  }
+  
+  .logo {
+    height : 30vh;
+  }
+
 </style>
 
--- components/AppNavigation.vue
+++ components/AppNavigation.vue
@@ -4,17 +4,18 @@
       <div class="navbar-brand">
         
         <span class="navbar-item is-hidden-desktop">
-          <img alt="Logo Yineo" src="~assets/images/logo-yineo.png" /> <span id="company-name">Y I N E O</span>
+          <img alt="Logo Yineo" src="~/assets/src/Yineo_logo_mobile.svg" class="logo-mobile" />
         </span>
-
-        <div @click="setMenuMobileIsOpened" :class="{'is-active': this.$store.state.menuMobileIsOpened}" class="navbar-burger" data-target="app-menu">
+        
+        <div @click="setMenuMobileIsOpened" :class="{'is-active': this.$store.state.menuMobileIsOpened}"
+             class="navbar-burger" data-target="app-menu">
           <span></span>
           <span></span>
           <span></span>
         </div>
-
+      
       </div>
-
+      
       <!-- This "nav-menu" is hidden on mobile -->
       <!-- Add the modifier "is-active" to display it on mobile -->
       <div id="app-menu" class="navbar-menu" :class="{'is-active': this.$store.state.menuMobileIsOpened}">
@@ -24,57 +25,61 @@
           <nuxt-link to="/contact" class="navbar-item"> Contact </nuxt-link>
         </div>
       </div>
-
+    
     </nav>
   </div>
 </template>
 
 <script>
-export default {
-  methods: {
-    setMenuMobileIsOpened () {
-      console.log(this.$store.state)
-      this.$store.commit('setMenuMobileIsOpened', !this.$store.state.menuMobileIsOpened)
+    export default {
+        methods: {
+            setMenuMobileIsOpened () {
+                console.log(this.$store.state)
+                this.$store.commit('setMenuMobileIsOpened', !this.$store.state.menuMobileIsOpened)
+            }
+        }
     }
-  }
-}
 </script>
 
 <style scoped>
-
-#company-name {
-  position:relative;
-  top:-2px;
-  left:+4px;
-  font-weight: bold;
-}
-
-.navbar-end {
-  margin: auto !important;
-}
-
-/* Create slide animation on mobile */
-@media screen and (max-width: 999px) {
-
-  .navbar-end {
-    padding:0.5rem 1rem;
-  }
-
+  
   .navbar-menu {
-    display: block;
-    overflow: hidden;
-    max-height: 0px;
-    padding: 0rem 1rem;
-    transition: max-height 0.2s;
+    border-style : solid;
+    border-color : #efefef;
+    border-width : thin 0;
   }
-
-  .navbar-menu.is-active {
-    max-height: 100vh;
-    transition: max-height 0.5s;
+  
+  .navbar-end {
+    margin : auto !important;
   }
-}
-
-.nuxt-link-exact-active {
-  color: #363636;
-}
+  
+  /* Create slide animation on mobile */
+  @media screen and (max-width : 999px) {
+    
+    .navbar-end {
+      padding      : 0.5rem 1rem;
+    }
+    
+    .navbar-menu {
+      display    : block;
+      overflow   : hidden;
+      max-height : 0;
+      padding    : 0 1rem;
+      transition : max-height 0.2s;
+    }
+    
+    .navbar-menu.is-active {
+      max-height : 100vh;
+      transition : max-height 0.5s;
+    }
+  }
+  
+  .navbar-item {
+    font-size : 1rem;
+    padding   : 1rem 2rem;
+  }
+  
+  .nuxt-link-exact-active {
+    color : #363636;
+  }
 </style>
