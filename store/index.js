c1a5b63a509c0b0478958d76594004e057e6614f
Jason Awbrey <jason.s.awbrey@gmail.com>
2017-09-03 01:54:32 -0700

    Update store/index.js to leverage Nuxt conventions
    By simply creating `store/index.js`, Nuxt will automatically import Vuex. You can do more with less if you want :)
    See: https://nuxtjs.org/guide/vuex-store

--- store/index.js
+++ store/index.js
@@ -1,21 +1,16 @@
-import Vuex from 'vuex'
-
 /**
  * This our global state for our app.
  */
-const createStore = () => new Vuex.Store({
-  state: {
-    menuMobileIsOpened: false,
-    postsSidebar: []
-  },
-  mutations: {
-    setMenuMobileIsOpened (state, value) {
-      state.menuMobileIsOpened = value
-    },
-    setPostsSidebar (state, value) {
-      state.postsSidebar = value
-    }
-  }
+export const state = () => ({
+  menuMobileIsOpened: false,
+  postsSidebar: []
 })
 
-export default createStore
+export const mutations = {
+  setMenuMobileIsOpened (state, value) {
+    state.menuMobileIsOpened = value
+  },
+  setPostsSidebar (state, value) {
+    state.postsSidebar = value
+  }
+}
